0db84954b5ef86d49ce85aa8bf7a712162fd0e68
Ferr0x <loreferro01@gmail.com>
2023-10-10 16:49:56 +0200

    inserted images the buffer overflow continued

---
+++ example1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char buff[30]; 
+    printf("Insert your name: ");
+    scanf("%50s", buff); 
+    printf("Hello, %s!\n", buff);
+    return 0;
+}
+
+//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPPBBBBBBBBCCCCCCCC
